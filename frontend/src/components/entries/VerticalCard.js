f4340aba1796846e636f6eeecdf24aca2f147a70
aleegs <aleej.gs@gmail.com>
2021-02-15 18:09:43 -0300

    Added onPress callback to verticalcard

--- frontend/src/components/entries/VerticalCard.js
+++ frontend/src/components/entries/VerticalCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View} from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { Badge, Right, Card, CardItem, Thumbnail, Text, Icon, Left, Body } from 'native-base';
 import Tag from '../Tag.js'
 
@@ -8,38 +8,40 @@ export default function VerticalCard(props) {
     const navigation = useNavigation();
 
     return (
-        <Card style={[{flex: 1}, props.featured && {borderColor:'#FFB400', borderLeftWidth:10}]} onPress={() => console.log("test") }>
-            <CardItem style={props.featured && {backgroundColor:'#FFEEC7'}}>
-                <Left>
-                    <Thumbnail source={{uri: props.item.image}} />
-                    <Body>
-                        <Text>{props.item.username}</Text>
-                        <Text note>{props.item.type} / {props.item.category}</Text> 
-                    </Body>
-                </Left>
+        <TouchableOpacity activeOpacity={0.5}  onPress={() => navigation.navigate('Details', props.item)}>
+            <Card style={[{flex: 1}, props.featured && {borderColor:'#FFB400', borderLeftWidth:10}]}>
+                <CardItem style={props.featured && {backgroundColor:'#FFEEC7'}}>
+                    <Left>
+                        <Thumbnail source={{uri: props.item.image}} />
+                        <Body>
+                            <Text>{props.item.username}</Text>
+                            <Text note>{props.item.type} / {props.item.category}</Text> 
+                        </Body>
+                    </Left>
 
-                {props.item.featured &&
-                <Right style={{zIndex:5, position:'absolute', right:'0%', top:'-10%'}}>
-                    <Badge style={{flexDirection:'row', backgroundColor:'#FFB400'}}>
-                            <Icon name="star" style={{ fontSize: 15, color: "#fff", lineHeight: 20 }}/>
-                            <Text style={{ color: 'white' }}>Featured</Text>
-                    </Badge>
-                </Right>}
+                    {props.item.featured &&
+                    <Right style={{zIndex:5, position:'absolute', right:'0%', top:'-10%'}}>
+                        <Badge style={{flexDirection:'row', backgroundColor:'#FFB400'}}>
+                                <Icon name="star" style={{ fontSize: 15, color: "#fff", lineHeight: 20 }}/>
+                                <Text style={{ color: 'white' }}>Featured</Text>
+                        </Badge>
+                    </Right>}
 
-            </CardItem>
-            <CardItem style={props.featured && {backgroundColor:'#FFEEC7'}}>
-            <Body style={{}}>
-                <Text>
-                {props.item.title}
-                </Text>               
+                </CardItem>
+                <CardItem style={props.featured && {backgroundColor:'#FFEEC7'}}>
+                <Body style={{}}>
+                    <Text>
+                    {props.item.title}
+                    </Text>               
 
-                <View style={{flex:1, flexDirection:'row', marginTop:5, flexWrap:'wrap', alignItems:'center',padding:10}}>
-                    <Tag icon="thumb-up">{props.item.likes}</Tag>
-                    <Tag icon="thumb-down">{props.item.dislikes}</Tag>
-                    <Tag style={{alignSelf: 'flex-start'}} icon="account">{props.item.members}</Tag>
-                </View>
-            </Body>
-            </CardItem>
-        </Card>
+                    <View style={{flex:1, flexDirection:'row', marginTop:5, flexWrap:'wrap', alignItems:'center',padding:10}}>
+                        <Tag icon="thumb-up">{props.item.likes}</Tag>
+                        <Tag icon="thumb-down">{props.item.dislikes}</Tag>
+                        <Tag style={{alignSelf: 'flex-start'}} icon="account">{props.item.members}</Tag>
+                    </View>
+                </Body>
+                </CardItem>
+            </Card>
+        </TouchableOpacity>
     )
 }
--- frontend/src/screens/EntryDetailsScreen.js
+++ frontend/src/screens/EntryDetailsScreen.js
@@ -3,22 +3,11 @@ import { Button, Text, View } from 'react-native';
 
 export default function EntryDetailsScreen({route, navigation}) {
     const data = route.params;
+    //        { data && <Text>{JSON.stringify(data)}</Text>}
     
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => navigation.push('Details')}
-        />
-        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-        <Button
-          title="Go back to first screen in stack"
-          onPress={() => navigation.popToTop()}
-        />
-
-        { data && <Text>{JSON.stringify(data)}</Text>}
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', borderTopColor:'black', borderTopStartRadius:0, borderTopEndRadius:0, borderTopWidth:100 }}>
+        
       </View>
     );
   }
