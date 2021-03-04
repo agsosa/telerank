86c9cf2884ef1f803d53f25e3abc06a595d519c6
aleegs <aleej.gs@gmail.com>
2021-03-04 05:04:59 -0300

    Update HorizontalCard

--- frontend/src/components/entries/HorizontalCard.jsx
+++ frontend/src/components/entries/HorizontalCard.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import { Card, Caption } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import { PropTypes } from 'prop-types';
 import { truncateWithEllipses, formatLanguageCode } from '../../lib/Helpers';
-import Tag from '../Tag';
+import NumberTag from '../NumberTag';
 
 import { colors } from '../../config/Styles';
 import FeaturedBadge from './FeaturedBadge';
 
 const styles = StyleSheet.create({
 	caption: { alignSelf: 'center' },
-	card: { minWidth: '100%', width: '100%' },
+	card: { marginBottom: 15 },
 	cardContent: { marginTop: 7 },
-	cardContentView: { flex: 1, flexDirection: 'row', marginTop: 5, flexWrap: 'wrap', justifyContent: 'space-between', padding: 10 },
-	cardFeatured: { backgroundColor: colors.featuredLight, borderColor: colors.featured, borderLeftWidth: 10 },
+	cardContentView: { flex: 1, flexDirection: 'row', marginTop: 5, flexWrap: 'wrap', justifyContent: 'space-between', padding: 5 },
+	cardFeatured: { backgroundColor: colors.featuredLight, borderColor: colors.featured, borderLeftWidth: 5 },
 	coverImg: { resizeMode: 'cover', width: '100%' },
+	featuredBadgeContainer: { position: 'absolute', right: 0, zIndex: 1 },
 	mainView: { flex: 1, padding: 5 },
 	membersCount: { alignSelf: 'flex-start' },
 });
@@ -23,7 +24,31 @@ const styles = StyleSheet.create({
 export default function HorizontalCard({ item }) {
 	const navigation = useNavigation();
 
-	return <View style={styles.mainView} />;
+	return (
+		<View style={styles.mainView}>
+			<TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate('Details', item)}>
+				<Card elevation={2} style={[styles.card, item.featured && styles.cardFeatured]}>
+					<Card.Title title={item.username} subtitle={`${item.type} / ${item.category} / ${formatLanguageCode(item.language)}`} />
+					<Card.Cover source={{ uri: item.image }} style={styles.coverImg} />
+
+					{item.featured && (
+						<View style={styles.featuredBadgeContainer}>
+							<FeaturedBadge />
+						</View>
+					)}
+
+					<Card.Content style={styles.cardContent}>
+						<Caption style={styles.caption}>{truncateWithEllipses(item.title, 32)}</Caption>
+						<View style={styles.cardContentView}>
+							<NumberTag icon='thumb-up' number={item.likes} />
+							<NumberTag icon='thumb-down' number={item.dislikes} />
+							<NumberTag style={styles.membersCount} icon='account' number={item.members} />
+						</View>
+					</Card.Content>
+				</Card>
+			</TouchableOpacity>
+		</View>
+	);
 }
 
 HorizontalCard.propTypes = {
