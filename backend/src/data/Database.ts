45eb8389f5698f3b6cd962d4e82a44fd6b0af1b9
aleegs <aleej.gs@gmail.com>
2021-03-11 17:34:38 -0300

    Fixed mongo/mongoose deprecations

--- backend/src/data/Database.ts
+++ backend/src/data/Database.ts
@@ -3,14 +3,26 @@ import mongoose from "mongoose";
 const mongoURL = "mongodb://localhost/telerank";
 
 export default function InitializeDatabase(): void {
-  mongoose.connect(mongoURL, (err) => {
-    if (err) {
-      console.error(`mongoose.connect failed on startup - retrying\n${err}`);
-      setTimeout(InitializeDatabase, 3000);
-    } else {
-      mongoose.Promise = global.Promise;
-      const db = mongoose.connection;
-      db.on("error", console.error.bind(console, "MongoDB connection error: "));
+  mongoose.connect(
+    mongoURL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    },
+    (err) => {
+      if (err) {
+        console.error(`mongoose.connect failed on startup - retrying\n${err}`);
+        setTimeout(InitializeDatabase, 3000);
+      } else {
+        mongoose.Promise = global.Promise;
+        const db = mongoose.connection;
+        db.on(
+          "error",
+          console.error.bind(console, "MongoDB connection error: ")
+        );
+      }
     }
-  });
+  );
 }
