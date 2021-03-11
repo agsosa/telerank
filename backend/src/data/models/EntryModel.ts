6df42f902cbb0c7a99da4a75caa7196cf4605053
aleegs <aleej.gs@gmail.com>
2021-03-11 15:39:54 -0300

    EntryModel converted to TypeScript

--- backend/src/data/models/EntryModel.ts
+++ backend/src/data/models/EntryModel.ts
@@ -1,6 +1,4 @@
-import mongoose, { Schema } from "mongoose";
-
-// TODO: EnumCategory, EnumType, EnumLanguage, etc
+import { model, Schema, Model, Document } from "mongoose";
 
 const EntryModelSchema = new Schema({
   username: { type: String, required: true, unique: true },
@@ -11,8 +9,8 @@ const EntryModelSchema = new Schema({
   description: { type: String, required: false },
   members: { type: Number, required: false, default: 0 },
   image: { type: String, required: false },
-  created_date: { type: Date, required: false, default: Date.now },
-  updated_date: { type: Date, required: false, default: Date.now },
+  createdDate: { type: Date, required: false, default: Date.now },
+  updatedDate: { type: Date, required: false, default: Date.now },
   likes: { type: Number, required: false, default: 0 },
   dislikes: { type: Number, required: false, default: 0 },
   featured: { type: Boolean, required: false, default: false },
@@ -22,4 +20,57 @@ const EntryModelSchema = new Schema({
   views: { type: Number, required: false, default: 0 },
 });
 
-export default mongoose.model("EntryModel", EntryModelSchema);
+export interface IEntry extends Document {
+  username: string;
+  type: string;
+  language: string;
+  category: string; // TODO: EnumCategory, EnumType, EnumLanguage, etc
+  title: string;
+  description: string;
+  members: number;
+  image: string;
+  createdDate: Date;
+  updatedDate: Date;
+  likes: number;
+  dislikes: number;
+  featured: boolean;
+  reports: number;
+  pending: boolean;
+  removed: boolean;
+  views: number;
+}
+
+export const EntryModel: Model<IEntry> = model("EntryModel", EntryModelSchema);
+
+export function GetAllEntries(): Promise<IEntry[]> {
+  return new Promise((resolve, reject) => {
+    EntryModel.find({}, (err, res: IEntry[]) => {
+      if (err) reject(err);
+      resolve(res);
+    });
+  });
+}
+
+export function AddEntry(obj: IEntry): Promise<IEntry> {
+  return new Promise((resolve, reject) => {
+    EntryModel.create(obj, (err, instance: IEntry) => {
+      if (err) reject(err);
+      resolve(instance);
+    });
+  });
+}
+
+export function ListEntries(perPage: number, page: number): Promise<IEntry[]> {
+  return new Promise((resolve, reject) => {
+    EntryModel.find()
+      .limit(perPage)
+      .skip(perPage * page)
+      .exec((err, entries: IEntry[]) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(entries);
+        }
+      });
+  });
+}
