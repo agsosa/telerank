8eb88ae121c62d738442ea1a4cb3a218a7349669
aleegs <aleej.gs@gmail.com>
2021-03-11 17:29:14 -0300

    Added StatsRoutes, cleanup

--- backend/App.ts
+++ backend/App.ts
@@ -5,19 +5,17 @@ import cors from "cors";
 import rateLimit from "express-rate-limit";
 import compression from "compression";
 import InitializeDatabase from "./src/data/Database";
-import { GetStats } from "./src/data/models/StatsModel";
 import CommonRoutesConfig from "./src/routes/common.routes.config";
 import EntriesRoutes from "./src/routes/entries.routes.config";
+import StatsRoutes from "./src/routes/stats.routes.config";
 
 // TODO: Para cada channel/bot/etc scrapeado subir la imagen a bucket de google storage ya que las URL de imagen de telegram expiran
 
 const port = 4001;
-
 const limiterOptions = rateLimit({
   windowMs: 10 * 60 * 1000,
-  max: 50,
+  max: 100,
 });
-
 const app = express();
 
 app.use(helmet());
@@ -28,14 +26,13 @@ app.use(bodyParser.json());
 // app.enable('trust proxy'); // reverse proxy (heroku, nginx) https://expressjs.com/en/guide/behind-proxies.html
 app.use(limiterOptions);
 
+// Routes
 const routes: CommonRoutesConfig[] = [];
 routes.push(new EntriesRoutes(app));
+routes.push(new StatsRoutes(app));
 
 InitializeDatabase();
-GetStats();
 // scraper_jobs.initialize();
-// database.AddEntry({username: "usuarioxd", type: "channel", language: "es", category: "cryptocurrencies", title: "loleta", description:"esta es una descripcion", members:50, image:"404.jpg", created_date: Date.now(), updated_date: Date.now(), likes: 50, dislikes: 150, featured: false})
-// database.GetAllEntries(null);
 
 app.get("/", (req, res) => {
   res.send("OK");
