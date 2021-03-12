758b40d3fd07fd61e9852a54f8e3d9b0c4b9cb4a
aleegs <aleej.gs@gmail.com>
2021-03-12 19:15:17 -0300

    sleep function

--- backend/src/lib/helpers.ts
+++ backend/src/lib/helpers.ts
@@ -2,3 +2,7 @@
 export function capitalizeStr(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
