2555301faafcc065304e650a708d2b4d57103bfb
aleegs <aleej.gs@gmail.com>
2021-03-13 05:02:04 -0300

    Update GCloud.ts

--- backend/src/lib/GCloud.ts
+++ backend/src/lib/GCloud.ts
@@ -2,6 +2,8 @@ import { Storage } from "@google-cloud/storage";
 import sharp from "sharp";
 import imagemin from "imagemin";
 import imageminMozjpeg from "imagemin-mozjpeg";
+import fs from "fs";
+import { log } from "./Helpers";
 
 const storage = new Storage({
   keyFilename: "./serviceAccountKey.json",
@@ -11,34 +13,43 @@ const storage = new Storage({
 const bucket = storage.bucket("telerank-e9b37.appspot.com");
 
 /* Upload an image to a Google Cloud Storage bucket, by default it will resize and optimize the image before uploading it.
+ * the file will be uploaded as username.jpg
  * Returns the public url
  */
-export async function uploadImage(
-  path: string,
+export async function uploadPhoto(
+  photoBytes: Uint8Array,
+  username: string,
   optimize = true
 ): Promise<string | undefined> {
   try {
+    const PATH = `${username}.jpg`;
     if (optimize) {
       // Resize with sharp
       const width = 400;
       const height = 400;
-      const buffer = await sharp(path).resize(width, height).jpeg().toBuffer();
-      await sharp(buffer).toFile(path);
+      const buffer = await sharp(Buffer.from(photoBytes))
+        .resize(width, height)
+        .jpeg()
+        .toBuffer();
+      await sharp(buffer).toFile(PATH);
 
       // Optimize with imagemin/mozjpeg
-      await imagemin([path], {
+      await imagemin([PATH], {
         plugins: [imageminMozjpeg({ quality: 80 })],
-        destination: path,
+        destination: "./",
       });
     }
 
     // Upload file
-    const uploadResults = await bucket.upload(path, { gzip: true });
+    const uploadResults = await bucket.upload(PATH, { gzip: true });
+
+    // Delete local file
+    fs.unlink(PATH, (err) => err && log.error(`Delete file failed: ${err}`));
 
     // Return public url
     return uploadResults[0].publicUrl();
   } catch (err) {
-    console.log(err);
+    log.error(err);
     return undefined;
   }
 }
@@ -47,11 +58,11 @@ export async function uploadImage(
 export async function listBuckets() {
   try {
     const [buckets] = await storage.getBuckets();
-    console.log("Buckets:");
+    log.info("Buckets:");
     buckets.forEach((b) => {
-      console.log(b.name);
+      log.info(b.name);
     });
   } catch (err) {
-    console.log(err);
+    log.error(err);
   }
 }
