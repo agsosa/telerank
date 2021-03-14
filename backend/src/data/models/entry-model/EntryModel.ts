45d98fa9007545851321d000e67a4a19c5a821b0
aleegs <aleej.gs@gmail.com>
2021-03-14 07:12:04 -0300

    Update EntryModel

--- backend/src/data/models/entry-model/EntryModel.ts
+++ backend/src/data/models/entry-model/EntryModel.ts
@@ -2,13 +2,19 @@ import { model, Schema, Model, Document } from "mongoose";
 import { IEntry, IEntryDocument } from "./IEntry";
 import EnumLanguage from "./EnumLanguage";
 
+// TODO: Implement cache?
 // TODO: Implement EnumCategories
 
 const EntryModelSchema = new Schema({
   username: { type: String, required: true, unique: true },
-  type: { type: String, required: true },
-  language: { type: String, required: true, default: EnumLanguage.ENGLISH },
-  category: { type: String, required: true, default: "Other" },
+  type: { type: String, required: true, index: true },
+  language: {
+    type: String,
+    required: true,
+    default: EnumLanguage.ENGLISH,
+    index: true,
+  },
+  category: { type: String, required: true, default: "Other", index: true },
   title: { type: String, required: false },
   description: { type: String, required: false },
   members: { type: Number, required: false, default: 0 },
@@ -17,10 +23,10 @@ const EntryModelSchema = new Schema({
   updatedDate: { type: Date, required: false, default: Date.now },
   likes: { type: Number, required: false, default: 0 },
   dislikes: { type: Number, required: false, default: 0 },
-  featured: { type: Boolean, required: false, default: false },
-  reports: { type: Number, required: false, default: 0 },
-  pending: { type: Boolean, required: true, default: true },
-  removed: { type: Boolean, required: true, default: true },
+  featured: { type: Boolean, required: false, default: false, index: true },
+  reports: { type: Number, required: false, default: 0, index: true },
+  pending: { type: Boolean, required: true, default: true, index: true },
+  removed: { type: Boolean, required: true, default: true, index: true },
   views: { type: Number, required: false, default: 0 },
 });
 
@@ -29,14 +35,17 @@ export const EntryModel: Model<IEntryDocument> = model(
   EntryModelSchema
 );
 
-export function GetAll(): Promise<IEntry[]> {
+/* export function GetAll(): Promise<IEntry[]> {
   return new Promise((resolve, reject) => {
-    EntryModel.find({}, (err, res: IEntry[]) => {
-      if (err) reject(err);
-      resolve(res);
-    });
+    EntryModel.find(
+      {  },
+      (err, res: IEntry[]) => {
+        if (err) reject(err);
+        resolve(res);
+      }
+    );
   });
-}
+} */
 
 export async function Insert(obj: IEntry | IEntry[]): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -54,14 +63,27 @@ export async function Insert(obj: IEntry | IEntry[]): Promise<void> {
   });
 }
 
-export function GetList(perPage: number, page: number): Promise<IEntry[]> {
+export function GetList(
+  perPage: number,
+  page: number,
+  query: Record<string, unknown> = {}
+): Promise<IEntry[]> {
   return new Promise((resolve, reject) => {
-    EntryModel.find()
+    EntryModel.find({ pending: false, removed: false, ...query })
       .limit(perPage)
       .skip(perPage * page)
       .exec((err, entries) => {
         if (err) reject(err);
         resolve(entries);
       });
+  });
+}
+
+export function isUsernameSaved(username: string): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    EntryModel.exists({ username: username.toLowerCase() }, (err, res) => {
+      if (err) reject(err);
+      resolve(res);
+    });
   });
 }
