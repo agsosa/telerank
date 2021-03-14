1211c71f5fc765604df51c19e48bec497b233e07
aleegs <aleej.gs@gmail.com>
2021-03-14 01:37:09 -0300

    Update PopulateDatabaseJob

--- backend/src/scrapers/ScraperJobs.ts
+++ backend/src/scrapers/ScraperJobs.ts
@@ -1,10 +1,16 @@
 import { spawn } from "child_process";
+import { Document } from "mongoose";
 import IScrapedMedia from "./content-scrapers/IScrapedMedia";
-import TelegramChannelsMe from "./content-scrapers/telegramchannels.me.scraper";
+import scrapeTelegramChannelsMe from "./content-scrapers/telegramchannels.me.scraper";
 import * as EntryModel from "../data/models/entry-model/EntryModel";
 import { getTelegramInfo } from "./telegram-proto/TelegramProto";
 import { uploadPhoto } from "../lib/GCloud";
 import { log } from "../lib/Helpers";
+import { IEntry } from "../data/models/entry-model/IEntry";
+import ITelegramInfo from "./telegram-proto/ITelegramInfo";
+import EnumLanguage from "../data/models/entry-model/EnumLanguage";
+
+const isJobRunning = false; // True if a job is already running
 
 /* 
   PopulateDatabaseJob():
@@ -13,62 +19,76 @@ import { log } from "../lib/Helpers";
     if they're not already added.
 
     1) Execute the scrapers from content-scrapers to find new usernames and try to categorize them
-    2) Discard usernames already added to the database
+    2) Discard usernames already added to the database, discard usernames with scam=true
     3) --- For each scraped username:
+    
       - Get all the info (title, description, photo, etc) using the TelegramProto module
+      - Discard usernames with info.scam = true
       - Process the info (i.e. upload the photo to Google Cloud Storage)
-      - Construct a IEntry object
-    4) Save each IEntry object from (3) to the database (EntryModel)
+      - Add to the database (EntryModel)
 */
 export async function PopulateDatabaseJob(): Promise<void> {
-  // TODO: Test cases: Bot, Group, Channel, big group/channel, invalid, IScrapedMedia
-  const info = await getTelegramInfo("cordobabitcoin"); // dealtrackerbot, cordobabitcoin, unfolded
-  if (info) {
-    const imageUrl = await uploadPhoto(info.photoBytes, info.username);
-    log.info(imageUrl);
-  }
-
-  /* try {
+  try {
+    // eslint-disable-next-line
     console.time("PopulateDatabase");
 
-    let media: IScrapedMedia[] = [];
-    media = media.concat(await TelegramChannelsMe());
-    // const promises: Promise<any>[] = [];
+    const mediaList: IScrapedMedia[] = await scrapeTelegramChannelsMe();
+    const entryList: IEntry[] = [];
+    const uploadPromises: Promise<any>[] = [];
 
-    media.forEach(async (q) => {
-      await new Promise((resolve, reject) => {
-        console.log(`Executing telegram.py for ${q.username}`);
-        const pythonProcess = spawn("python", [
-          "../python/telegram.py",
-          q.username,
-        ]);
+    for (let i = 0; i < mediaList.length; i += 1) {
+      const q = mediaList[i];
 
-        pythonProcess.stdout.setEncoding("latin1");
-        pythonProcess.stdout.on("data", (res) => {
-          const data = JSON.parse(res);
-          if (!data || !data.members || Number.isNaN(data.members))
-            throw new Error(
-              `Received invalid data from Python ${data} for username ${q.username}`
-            );
+      // TODO: Check if username is already on database, if not continue
 
+      log.info(`Executing getTelegramInfo for ${q.username}`);
 
-          console.log(`${q.username} ${res}`);
+      // eslint-disable-next-line
+      const info = await getTelegramInfo(q.username); // !IMPORTANT: Wait for each getTelegramInfo() to avoid Telegram API limits
 
-          // database.AddEntry(q, () => resolve());
+      log.info("ok");
 
-          resolve(true);
-        }); // end pythonProcess.stdout.on
-      }); // end new Promise()
+      if (info && !info.scam) {
+        uploadPromises.push(
+          uploadPhoto(info.photoBytes, info.username).then((imgURL) => {
+            const entry: IEntry = {
+              username: info.username,
+              type: info.type,
+              language: q.language as EnumLanguage,
+              category: q.category,
+              title: info.title,
+              description: info.description,
+              members: info.members,
+              image: imgURL || "",
+              createdDate: info.creationDate,
+              updatedDate: new Date(),
+              likes: 0,
+              dislikes: 0,
+              featured: false,
+              reports: 0,
+              pending: true,
+              removed: false,
+              views: 0,
+            };
 
-      // promises.push(prom);
-    }); // end media.forEach()
+            entryList.push(entry);
+          })
+        );
+      }
+    }
 
-    // await Promise.all(promises);
+    await Promise.all(uploadPromises);
+    EntryModel.Insert(entryList);
 
+    // eslint-disable-next-line
     console.timeEnd("PopulateDatabase");
   } catch (err) {
-    console.log(`ScraperJobs: PopulateDatabase error: ${err}`);
-  } */
+    log.error(`PopulateDatabase error: ${err}`);
+  }
 }
 
-// export function RefreshAllTelegramInfosJob() {}
+export function RefreshAllTelegramInfosJob() {
+  throw new Error("NotImplemented");
+}
+
+// TODO: En UpdateSavedInfo revisar si ITelegramInfo scam field = true, en caso de ser true setear IEntry removed = true
