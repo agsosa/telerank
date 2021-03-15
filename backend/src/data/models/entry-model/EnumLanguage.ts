75805baf5447ec0e15edcbca6f8e88992de2d78c
aleegs <aleej.gs@gmail.com>
2021-03-15 01:59:13 -0300

    Update EnumLanguage

--- backend/src/data/models/entry-model/EnumLanguage.ts
+++ backend/src/data/models/entry-model/EnumLanguage.ts
@@ -4,4 +4,26 @@ enum EnumLanguage {
   ENGLISH = "English",
 }
 
+// Will try to get a EnumLanguage from a str. Returns undefined  if it couldn't parse the string.
+export function parseLanguage(str: string): EnumLanguage | undefined {
+  if (str) {
+    switch (str.toLowerCase()) {
+      case "ingles":
+      case "en":
+      case "english":
+      case "inglés":
+        return EnumLanguage.ENGLISH;
+      case "es":
+      case "spanish":
+      case "español":
+      case "espanol":
+      case "espaniol":
+        return EnumLanguage.SPANISH;
+      default:
+        return undefined;
+    }
+  }
+  return undefined;
+}
+
 export default EnumLanguage;
