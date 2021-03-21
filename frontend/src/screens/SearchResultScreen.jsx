58f6d80e37279dbfa34039e76207bb1e3fa16921
aleegs <aleej.gs@gmail.com>
2021-03-20 22:58:50 -0300

    Implemented SearchResultScreen

--- frontend/src/screens/SearchResultScreen.jsx
+++ frontend/src/screens/SearchResultScreen.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import { PropTypes } from 'prop-types';
+import VerticalList from '../components/entries/VerticalList';
+import { colors } from '../config/Styles';
 
 const styles = StyleSheet.create({
-	mainView: { alignItems: 'center', flex: 1, justifyContent: 'center' },
+	subtitleText: {
+		color: colors.grayAlt,
+		fontSize: 20,
+		marginVertical: 10,
+		textAlign: 'center',
+	},
 });
 
-export default function SearchResultScreen() {
-	return <View style={styles.mainView} />;
+function SearchResultScreen({ route }) {
+	const { data, searchText } = route.params;
+	console.log(data.length, searchText);
+	function HeaderRenderer() {
+		return (
+			<Text style={styles.subtitleText}>
+				{data.length} entries found for the term(s) &quot;{searchText}&quot;
+			</Text>
+		);
+	}
+	return <VerticalList Header={HeaderRenderer} useFilters useData={data} />;
 }
+
+SearchResultScreen.propTypes = {
+	route: PropTypes.object.isRequired,
+};
+
+export default SearchResultScreen;
