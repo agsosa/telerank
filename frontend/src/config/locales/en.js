0eb94d0cd55363cd706ab9aebb95565403ed3163
aleegs <aleej.gs@gmail.com>
2021-03-26 05:04:06 -0300

    Update locales

--- frontend/src/config/locales/en.js
+++ frontend/src/config/locales/en.js
@@ -1,12 +1,27 @@
 export default {
-	errorOccurred: 'An error occurred, please try again.',
-	globalsearch: {
-		title: 'Discover your next community',
-		subtitle: 'Explore more than 5000 Telegram channels, groups, bots and stickers.',
-		random: 'Random',
+	translation: {
+		errorOccurred: 'An error occurred, please try again.',
+		globalsearch: {
+			title: 'Discover your next community',
+			subtitle: 'Explore more than 5000 Telegram channels, groups, bots and stickers.',
+			random: 'Random',
+		},
+		drawer: {
+			language: 'Language',
+			statistics: 'Statistics',
+			contact: 'Contact',
+		},
+		navtabs: {
+			home: 'Home',
+			featured: 'Featured',
+			channels: 'Channels',
+			groups: 'Groups',
+			bots: 'Bots',
+			stickers: 'Stickers',
+		},
+		search: 'Search',
+		searchNoResults: 'No results were found for your search.',
+		searchNothingFound: 'Nothing found',
+		searchTypeSomething: 'Please type something to search.',
 	},
-	search: 'Search',
-	searchNoResults: 'No results were found for your search.',
-	searchNothingFound: 'Nothing found',
-	searchTypeSomething: 'Please type something to search.',
 };
--- frontend/src/config/locales/es.js
+++ frontend/src/config/locales/es.js
@@ -1,12 +1,27 @@
 export default {
-	errorOccurred: 'Ha ocurrido un error, por favor intenta nuevamente.',
-	globalsearch: {
-		title: 'Descubre nuevas comunidades',
-		subtitle: 'Explora más de 5000 canales, grupos, bots y stickers de Telegram.',
-		random: 'Aleatorio',
+	translation: {
+		errorOccurred: 'Ha ocurrido un error, por favor intenta nuevamente.',
+		globalsearch: {
+			title: 'Descubre nuevas comunidades',
+			subtitle: 'Explora más de 5000 canales, grupos, bots y stickers de Telegram.',
+			random: 'Aleatorio',
+		},
+		drawer: {
+			language: 'Idioma',
+			statistics: 'Estadísticas',
+			contact: 'Contacto',
+		},
+		navtabs: {
+			home: 'Inicio',
+			featured: 'Destacado',
+			channels: 'Canales',
+			groups: 'Grupos',
+			bots: 'Bots',
+			stickers: 'Stickers',
+		},
+		search: 'Buscar',
+		searchNoResults: 'No se han encontrado resultados para tu búsqueda.',
+		searchNothingFound: 'Nada encontrado',
+		searchTypeSomething: 'Por favor escribe un término para buscar.',
 	},
-	search: 'Buscar',
-	searchNoResults: 'No se han encontrado resultados para tu búsqueda.',
-	searchNothingFound: 'Nada encontrado',
-	searchTypeSomething: 'Por favor escribe un término para buscar.',
 };
