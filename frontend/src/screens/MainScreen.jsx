e86e3a84337429c71c51b1b141a2abdcb6082887
aleegs <aleej.gs@gmail.com>
2021-03-26 05:02:50 -0300

    MainScreen i18n implementation

--- frontend/src/screens/MainScreen.jsx
+++ frontend/src/screens/MainScreen.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import HomeTab from './tabscreens/HomeTab';
 import APIErrorSnackbar from '../components/APIErrorSnackbar';
 import FeaturedTab from './tabscreens/FeaturedTab';
@@ -11,17 +12,18 @@ import BotsTab from './tabscreens/BotsTab';
 import ChannelsTab from './tabscreens/ChannelsTab';
 import NavTabs from '../components/navigation/NavTabs';
 
-const tabs = [
-	{ name: 'Inicio', icon: 'home', component: HomeTab },
-	{ name: 'Featured', icon: 'star', component: FeaturedTab },
-	{ name: 'Canales', icon: 'bullhorn', component: ChannelsTab },
-	{ name: 'Grupos', icon: 'forum', component: GroupsTab },
-	{ name: 'Bots', icon: 'robot', component: BotsTab },
-	{ name: 'Stickers', icon: 'sticker', component: StickersTab },
-];
-
 const MainScreen = ({ setDrawerNavigation }) => {
+	const { t } = useTranslation();
 	const navigation = useNavigation();
+
+	const tabs = [
+		{ name: t('navtabs.home'), icon: 'home', component: HomeTab },
+		{ name: t('navtabs.featured'), icon: 'star', component: FeaturedTab },
+		{ name: t('navtabs.channels'), icon: 'bullhorn', component: ChannelsTab },
+		{ name: t('navtabs.groups'), icon: 'forum', component: GroupsTab },
+		{ name: t('navtabs.bots'), icon: 'robot', component: BotsTab },
+		{ name: t('navtabs.stickers'), icon: 'sticker', component: StickersTab },
+	];
 
 	React.useEffect(() => {
 		setDrawerNavigation(navigation);
