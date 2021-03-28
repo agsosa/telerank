febacb94737e7c05caee020ca1778507e27132fd
aleegs <aleej.gs@gmail.com>
2021-03-28 17:08:25 -0300

    Stats i18next implementation

--- frontend/src/components/entries/Stats.jsx
+++ frontend/src/components/entries/Stats.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Text, StyleSheet, ScrollView } from 'react-native';
 import { Grid, Col, Row } from 'native-base';
+import { useTranslation } from 'react-i18next';
 import { formattedNumber, useIsMounted } from '../../lib/Helpers';
 import { colors } from '../../config/Styles';
 import LoadingIndicator from '../LoadingIndicator';
@@ -43,6 +44,7 @@ export default function Stats() {
 	const isMounted = useIsMounted();
 	const [data, setData] = useState({});
 	const [loading, setLoading] = useState(true);
+	const { t } = useTranslation();
 
 	const refreshData = async () => {
 		setLoading(true);
@@ -63,47 +65,47 @@ export default function Stats() {
 
 	return (
 		<ScrollView>
-			<Text style={styles.text}>{`We have ${data.channels + data.groups + data.bots + data.stickers} channels, groups, bots and stickers added to our directory.`}</Text>
+			<Text style={styles.text}>{t('stats.header', { total: data.channels + data.groups + data.bots + data.stickers })}</Text>
 			<Grid style={styles.grid}>
 				<Col style={styles.col}>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>Canales</Text>
+						<Text style={styles.row_title}>{t('navTabs.channels')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.channels)}</Text>
 					</Row>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>Grupos</Text>
+						<Text style={styles.row_title}>{t('navTabs.groups')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.groups)}</Text>
 					</Row>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>En revision</Text>
+						<Text style={styles.row_title}>{t('stats.pending')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.pending)}</Text>
 					</Row>
 				</Col>
 				<Col style={styles.col}>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>Bots</Text>
+						<Text style={styles.row_title}>{t('navTabs.bots')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.bots)}</Text>
 					</Row>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>Stickers</Text>
+						<Text style={styles.row_title}>{t('navTabs.stickers')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.stickers)}</Text>
 					</Row>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>Eliminados</Text>
+						<Text style={styles.row_title}>{t('stats.removed')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.removed)}</Text>
 					</Row>
 				</Col>
 				<Col style={styles.col}>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>Members</Text>
+						<Text style={styles.row_title}>{t('stats.members')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.members)}</Text>
 					</Row>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>Views</Text>
+						<Text style={styles.row_title}>{t('stats.views')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.views)}</Text>
 					</Row>
 					<Row style={styles.row}>
-						<Text style={styles.row_title}>Ratings</Text>
+						<Text style={styles.row_title}>{t('stats.ratings')}</Text>
 						<Text style={styles.row_number}>{formattedNumber(data.ratings)}</Text>
 					</Row>
 				</Col>
