8ff2dbf805bbc9dea8d9f243abff2cf2364cb452
aleegs <aleej.gs@gmail.com>
2021-03-28 16:48:20 -0300

    StatsModal i18next implementation

--- frontend/src/components/modals/StatsModal.jsx
+++ frontend/src/components/modals/StatsModal.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Text } from 'react-native';
 import { Button, Dialog, Portal } from 'react-native-paper';
+import { useTranslation } from 'react-i18next';
 import Stats from '../entries/Stats';
 
 const StatsModal = React.forwardRef((props, ref) => {
 	const [visible, setVisible] = React.useState(false);
+	const { t } = useTranslation();
 
 	const show = () => {
 		setVisible(true);
@@ -26,14 +28,14 @@ const StatsModal = React.forwardRef((props, ref) => {
 		<Portal>
 			<Dialog visible={visible} onDismiss={hide}>
 				<Dialog.Title>
-					<Text>Statistics</Text>
+					<Text>{t('drawer.statistics')}</Text>
 				</Dialog.Title>
 				<Dialog.ScrollArea>
 					<Stats />
 				</Dialog.ScrollArea>
 				<Dialog.Actions>
 					<Button onPress={hide}>
-						<Text>Done</Text>
+						<Text>{t('done')}</Text>
 					</Button>
 				</Dialog.Actions>
 			</Dialog>
