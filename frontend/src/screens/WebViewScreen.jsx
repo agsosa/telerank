6e359406ba5c50d96e263c97da0342f522dbe151
aleegs <aleej.gs@gmail.com>
2021-03-28 18:17:24 -0300

    Update WebViewScreen

--- frontend/src/screens/WebViewScreen.jsx
+++ frontend/src/screens/WebViewScreen.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
 import { PropTypes } from 'prop-types';
+import LoadingIndicator from '../components/LoadingIndicator';
+
+const RenderLoading = () => <LoadingIndicator />;
 
 export default function WebViewScreen({ route }) {
 	const { url } = route.params;
-	return <WebView source={{ uri: url }} />;
+	return <WebView startInLoadingState renderLoading={RenderLoading} source={{ uri: url }} />;
 }
 
 WebViewScreen.propTypes = {
