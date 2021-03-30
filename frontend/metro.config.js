1dd94704d97246c09e1ee7e5e98c834f87dfb50e
aleegs <aleej.gs@gmail.com>
2021-03-29 21:18:35 -0300

    Added metro-symlinked-deps

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,8 +1,14 @@
-module.exports = {
-	resolver: {
-		sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+/* eslint-disable */
+const { applyConfigForLinkedDependencies } = require('@carimus/metro-symlinked-deps');
+
+module.exports = applyConfigForLinkedDependencies(
+	{
+		resolver: {
+			sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+		},
+		transformer: {
+			assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+		},
 	},
-	transformer: {
-		assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-	},
-};
+	{ projectRoot: __dirname }
+);
